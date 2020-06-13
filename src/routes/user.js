138eb5a404747a6249515f576c4683c901ac6b2a
Jonathan Mayol <mayoljonathan@gmail.com>
2020-06-13 17:42:15 +0800

    remove swagger comment

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,21 +4,6 @@ const { UserController } = require('../controllers');
 
 const router = express.Router();
 
-/**
- * @swagger
- * /user/login:
- *  post:
- *    tags:
- *      - user
- *    name: Login
- *    summary: Login an user
- *    description: Use to login an user
- *
- *
- *    responses:
- *      '200':
- *        description: Successful login
- */
 router
   .post('/v1/user', UserController.register)
   .post('/v1/user/login', UserController.login)
