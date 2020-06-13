b0ec48efd18d3c164b93d7001ce8b2a23ac3452e
Jonathan Mayol <mayoljonathan@gmail.com>
2020-06-13 17:59:12 +0800

    added whitelisted origin

---
+++ src/config/whitelisted_origins.js
@@ -0,0 +1,3 @@
+const whitelistedOrigins = ['https://editor.swagger.io', 'https://mayoljonathan.github.io'];
+
+module.exports = { whitelistedOrigins };
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -4,6 +4,7 @@ const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = swaggerJsDoc(require('../config/swaggerOptions.json'));
 const { passport } = require('../config/passport');
+const { whitelistedOrigins } = require('../config/whitelisted_origins');
 
 const api = require('../routes');
 const { ResponseHandler } = require('../utils/response_hander');
@@ -17,7 +18,17 @@ module.exports = (expressApp) => {
   expressApp.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
   expressApp.use('/api/', api);
 
-  expressApp.use(cors());
+  expressApp.use(
+    cors({
+      origin: (origin, cb) => {
+        if (whitelistedOrigins.indexOf(origin) === -1) {
+          const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+          return cb(new Error(msg), false);
+        }
+        return cb(null, true);
+      },
+    })
+  );
 
   expressApp.use((req, res) => {
     res.send(ResponseHandler.error('Invalid request. Please check the docs for information.'));
